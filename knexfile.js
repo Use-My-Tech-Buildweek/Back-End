aab3e19c0d96e68dc4442fc1c85cb31c1a171880
MarkLWorkman <70355546+MarkLWorkman@users.noreply.github.com>
2021-06-16 23:56:44 -0400

    database made

--- knexfile.js
+++ knexfile.js
@@ -1,40 +1,26 @@
-require("dotenv").config();
-
-const pg = require("pg");
-
-if (process.env.DATABASE_URL) {
-  pg.defaults.ssl = { rejectUnauthorized: false };
-}
-
 const sharedConfig = {
-  client: "pg",
-  migrations: { directory: "./data/migrations" },
-  seeds: { directory: "./data/seeds" },
+  client: "sqlite3",
+  useNullAsDefault: true,
+  migrations: {
+    directory: "./data/migrations",
+  },
+  seeds: {
+    directory: "./data/seeds",
+  },
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run("PRAGMA foreign_keys = ON", done);
+    },
+  },
 };
 
 module.exports = {
   development: {
     ...sharedConfig,
-    connection: {
-      host: "localhost",
-      port: 5432,
-      user: "postgres",
-      password: "postgres",
-      database: "UseMyTech",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    useNullAsDefault: true,
+    connection: { filename: "./data/useMyTech.db3" },
   },
   testing: {
     ...sharedConfig,
-    connection: process.env.TESTING_DATABASE_URL,
-  },
-  production: {
-    ...sharedConfig,
-    connection: process.env.DATABASE_URL,
-    pool: { min: 2, max: 10 },
+    connection: { filename: "./data/testing.db3" },
   },
 };
