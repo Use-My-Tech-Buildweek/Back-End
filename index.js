4057fd2cdc595e04ebfc7d69f630ca9f10baaa5f
MarkLWorkman <70355546+MarkLWorkman@users.noreply.github.com>
2021-06-17 23:46:28 -0400

    test again

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-require("dotenv").config();
-
-const path = require("path");
-const express = require("express");
-
 const server = require("./api/server");
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 5000;
 
-server.use(express.static(path.join(__dirname, "client/dist")));
-
-server.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
-});
-
-server.listen(PORT, () => {
-  console.log(`server listening on ${PORT}`);
+server.listen(port, () => {
+  console.log(`server is runnning on port:${port}`);
 });
